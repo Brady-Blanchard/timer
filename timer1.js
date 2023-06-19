8b373ba4496e0da216247129bb4bdc8fb25ae769
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-19 16:32:41 -0300

    Add edge cases

--- timer1.js
+++ timer1.js
@@ -1,6 +1,19 @@
 for (let i = 2; i < process.argv.length; i++) {
-  setTimeout(() => {
-    process.stdout.write('\u0007');
-    console.log('alarm!')
-  }, process.argv[i] * 1000)
+  if (process.argv[i] < 0 || isNaN(process.argv[i])) {
+  } else {
+    setTimeout(() => {
+      process.stdout.write('\u0007');
+    }, process.argv[i] * 1000);
+  }
 }
+
+// TEST CODE
+
+for (let i = 0; i <= 15; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r${i} seconds`);
+  }, i * 1000)
+}
+setTimeout(() => {
+  process.stdout.write(`\n`);
+}, 16 * 1000);
