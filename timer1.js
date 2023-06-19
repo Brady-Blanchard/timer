c90817085866945789fd913c6572303abfa5bdf9
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-19 16:35:44 -0300

    Comment out test code

--- timer1.js
+++ timer1.js
@@ -1,5 +1,6 @@
 for (let i = 2; i < process.argv.length; i++) {
   if (process.argv[i] < 0 || isNaN(process.argv[i])) {
+    // if it is a negative number or Not a Number do nothing
   } else {
     setTimeout(() => {
       process.stdout.write('\u0007');
@@ -9,11 +10,13 @@ for (let i = 2; i < process.argv.length; i++) {
 
 // TEST CODE
 
-for (let i = 0; i <= 15; i++) {
-  setTimeout(() => {
-    process.stdout.write(`\r${i} seconds`);
-  }, i * 1000)
-}
-setTimeout(() => {
-  process.stdout.write(`\n`);
-}, 16 * 1000);
+// counter for keeping track of seconds
+
+// for (let i = 0; i <= 15; i++) {
+//   setTimeout(() => {
+//     process.stdout.write(`\r${i} seconds`);
+//   }, i * 1000);
+// }
+// setTimeout(() => {
+//   process.stdout.write(`\n`);
+// }, 16 * 1000);
